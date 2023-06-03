221f381261d02c6281107e84a9304f84b0994282
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2023-06-03 12:53:36 +0200

    issue detected and resolved, image-data flex direction done

--- src/components/DataImage.jsx
+++ src/components/DataImage.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import React from "react";
 
-function DataImage({title, description,langs, collaborators, link,linkGitClient, linkGitServer, picture}) {
+function DataImage({
+  title,
+  description,
+  langs,
+  collaborators,
+  link,
+  linkGitClient,
+  linkGitServer,
+  picture,
+}) {
   return (
-    <div>
-         <div className='project-image-data' style={{display:"flex"}}>
-        <div className='text-project2'>
-       <h2 className='title-h2'>{title}</h2>
-       <p>{description}</p>
-       <p>{langs}</p>
-       {collaborators && <h5>Collaborators: {collaborators}</h5>}
-       {link && <a href={link} target="blank">Visite: {title}</a>}
-       {linkGitClient && <a href={linkGitClient} target="blank">GitHub Client</a>}
-       <br />
-       {linkGitServer && <a href={linkGitServer} target="blank">GitHub Server</a>}
-        </div>
-        <div>
-        <a href={link} target="blank" >
-           <img src={picture} alt="" className="grow-image"/>
+    <div className="project-image-data">
+      <div className="text-project2">
+        <h2 className="title-h2">{title}</h2>
+        <p>{description}</p>
+        <p>{langs}</p>
+        {collaborators && <h5>Collaborators: {collaborators}</h5>}
+        {link && (
+          <a href={link} target="blank">
+            Visite: {title}
+          </a>
+        )}
+        {linkGitClient && (
+          <a href={linkGitClient} target="blank">
+            GitHub Client
+          </a>
+        )}
+        <br />
+        {linkGitServer && (
+          <a href={linkGitServer} target="blank">
+            GitHub Server
+          </a>
+        )}
+      </div>
+      <div className="image-project">
+        <a href={link} target="blank">
+          <img src={picture} alt="" className="grow-image" />
         </a>
-        </div>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default DataImage
+export default DataImage;
--- src/components/ImageData.jsx
+++ src/components/ImageData.jsx
@@ -7,8 +7,8 @@ function ImageData(props) {
     
    const {title, description,langs, collaborators, link,picture,linkGitClient,linkGitServer} = props
   return (
-    <div className='project-image-data' id= "data-image" style={{display:"flex" }}>
-        <div>
+    <div className='project-image-data' id= "data-image" >
+        <div className="image-project">
         <a href={link} target="blank" >
            <img src={picture} alt="" className="grow-image" />
         </a>
